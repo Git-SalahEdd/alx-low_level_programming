6ffb64a247257a0a67df73ebea9c8f9a94c6be36
salah <salah@email.com>
2023-06-16 12:30:23 -0700

    task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i != 'z')
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
