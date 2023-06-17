0556f53c4b523db26303903afb81ccdb2f80108d
salah <salah@email.com>
2023-06-16 18:38:46 -0700

    done task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 10; i++) 
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i == 8 && j == 9)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
