36d0421ecbf9b5faea32baacc6dc0e2ad296acde
salah <salah@email.com>
2023-06-20 11:51:05 -0700

    done task 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_islower - check for lowcase
+*Return: return 1 if c lowcase or 0 otherwise
+*/
+
+int _islower(int c)
+{
+	int rst = c;
+
+	if (rst >= 97 && rst <=122)
+	{
+		rst = 1;
+	}
+	if (rst >=65 && rst <= 90)
+	{
+		rst = 0;
+	}
+	return rst;
+}
