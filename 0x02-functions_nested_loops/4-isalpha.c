4263280139204c37294cb2da640f5aac5fa7e1e5
salah <salah@email.com>
2023-06-20 12:37:59 -0700

    done task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+* _isalpha: - check a char if isalpha
+* Return: always rst result to check if it's alpha
+* @c: parameter (int) to check if alpha or not
+*/
 
 int _isalpha(int c)
 {
 	int rst = c;
 
-	if(rst >= 65 && rst <= 122)
+	if (rst >= 65 && rst <= 122)
 	{
 		rst = 1;
 	}
@@ -13,5 +18,5 @@ int _isalpha(int c)
 	{
 		rst = 0;
 	}
-	return rst;
+	return (rst);
 }
