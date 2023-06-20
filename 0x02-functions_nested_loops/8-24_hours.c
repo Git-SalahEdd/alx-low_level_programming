46e05c8b70d1daad376b1f6241b470153d861221
salah <salah@email.com>
2023-06-20 14:11:06 -0700

    done task 8 lets gooo

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - prints every minute 
+ * of the day of Jack Bauer, 
+ * starting from 00:00 to 23:59
  */
 
 void jack_bauer(void)
 {
-	int min,hrs;
+	int min, hrs;
 
 	for (hrs = 0; hrs <= 23; hrs++)
 	{
