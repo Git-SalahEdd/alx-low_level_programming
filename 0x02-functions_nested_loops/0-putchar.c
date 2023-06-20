b78869a03b71dce27bb530173ded3ea119ee433a
salah <salah@email.com>
2023-06-20 08:19:26 -0700

    done task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,2 @@
-#include <stdio.h>
-/**
-*main - Entry point
-*Return: always 0 (success)
-*/
-
-int main(void)
-{
-	printf("_putchar\n");
-	return (0);
-}
+#include "main.h"
+	_putchar(_putchar\n);
