ef8cd4ef9bed51b5d4c6a49319c03ce5b00ec791
salah <salah@email.com>
2023-06-20 13:33:38 -0700

    done task 6 lets gooo

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,7 +9,7 @@
 int _abs(int abs)
 {
 	int abs2 = abs;
-	
+
 	if (abs2 < 0)
 	{
 		return (-abs2);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,13 @@
-sd
+#include "main.h"
+
+/**
+ * print_last_digit - check
+ * Return: return 1 if c is lowercase, 0 otherwise
+ * @ : parameter (int) or not
+ */
+
+int print_last_digit(int ld)
+{
+int x = ld;
+
+}
