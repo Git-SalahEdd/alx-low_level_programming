6c115d46ecfe0e87a8aa8fcaa358152b41a67342
salah <salah@email.com>
2023-06-20 16:08:42 -0700

    done task 9 lets gooo

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * 9time table - print table 9 time
+ */
+
+void times_table(void)
+{
+	int a, b, calc;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+
+			calc = a * b;
+			if (calc <= 9)
+			{
+				_putchar(' ');
+				_putchar(calc + '0');
+			}
+			else
+			{
+				_putchar((calc / 10) + 48);
+				_putchar((calc % 10) + 48);
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+	_putchar('\n');
+	}
+}
