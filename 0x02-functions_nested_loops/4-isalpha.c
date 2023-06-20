4b9e3030302db4a24c233bb36d363098b5d4e411
salah <salah@email.com>
2023-06-20 12:39:33 -0700

    done task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _isalpha: - check a char if isalpha
-* Return: always rst result to check if it's alpha
-* @c: parameter (int) to check if alpha or not
-*/
+ * _isalpha - check for lowercase
+ * Return: return 1 if c is lowercase, 0 otherwise
+ * @c: parameter (int) to check if lowercase or not
+ */
 
 int _isalpha(int c)
 {
