1985907623692ded2b323014ba23561079e424ab
salah <salah@email.com>
2023-06-20 17:47:30 -0700

    done task 10  lets gooo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adding numbers
+ * @a: the parameter to be tested
+ * @b: the second parameter to be tested
+ * Return: result of a+b
+ */
+
+int add(int a,int b)
+{
+return (a+b);
+}
