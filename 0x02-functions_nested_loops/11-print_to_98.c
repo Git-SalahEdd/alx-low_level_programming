ae93f99076bd7a15943be79aba8dca850d525a5e
salah <salah@email.com>
2023-06-20 17:55:41 -0700

    done task 11  lets gooo

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print int from 0 to 98
+ * @nbr: parameter to be tested
+ * Return: Always 0.
+ */
+
+void print_to_98(int nbr)
+{
+	int nr = nbr;
+
+	if (nr <= 98)
+	{
+		for (; nr <= 98; nr++)
+		{
+			printf("%d", nr);
+			if (nr != 98)
+				printf(", ");
+		}
+    	}
+    	else
+	{
+        	for (; nr >= 98; nr--)
+		{
+            	printf("%d", nr);
+            	if (nr != 98)
+                	printf(", ");
+        	}
+    	}
+	printf("\n");
+}
