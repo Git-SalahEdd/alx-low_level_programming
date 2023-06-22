29ca61f87c8a60672be0a85a4bb393c9846aac63
salah <salah@email.com>
2023-06-22 15:43:54 -0700

    done task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - prog divby 3 fizz or div 5 buzz or both fizzbuzz
+ * Return: 0 always sucess
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
