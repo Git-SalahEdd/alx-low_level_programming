05fc7d22b1112f127cb9cf5179181387f714db41
salah <salah@email.com>
2023-06-22 10:52:37 -0700

    done task 5 using 3putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers 0-14
+ *
+ */
+
+void more_numbers(void)
+{
+	int var = 0;
+
+	while (var <= 10)
+	{
+		int var2 = 0;
+		
+		while (var2 <= 14)
+		{
+			if (var2 >= 10)
+			{
+				_putchar(var2 / 10 + '0');
+			}
+			_putchar(var2 % 10 + '0');
+			var2++;
+		}
+		var++;
+		_putchar('\n');
+	}
+}
