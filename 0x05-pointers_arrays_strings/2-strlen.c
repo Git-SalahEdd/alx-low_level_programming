f283238a5d3fcf60b953cdedaf715523f3142146
salah <salah@email.com>
2023-06-26 06:39:48 -0700

    done task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *@*s: parameter
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}
