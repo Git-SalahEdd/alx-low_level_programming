9a094d15c43b4ed29b98882cee1a5e44cb08ad7b
salah <salah@email.com>
2023-06-27 08:20:05 -0700

    done task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: 0 always sucess
+ */
+
+int main(void)
+{
+	int i = 0, j = 0;
+
+	while (i <= 98)
+	{
+		while (j <= 99)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10));
+			j++;
+		
+		i++;
+	if (i == 98 && j == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
