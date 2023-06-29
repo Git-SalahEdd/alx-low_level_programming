f64b3c5e16687c3babc892cc6a6c4a246b5242bd
salah <salah@email.com>
2023-06-29 06:54:46 -0700

    done task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,25 +2,28 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+**_strncat - concatenates two strings
+*@dest: parameter receive string from src
+*@src: parameter string to send to dest
+*@n: n bytes from src
+*Return: dest final string
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int j = 0;
 
-	while (dest[i])
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[j])
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
