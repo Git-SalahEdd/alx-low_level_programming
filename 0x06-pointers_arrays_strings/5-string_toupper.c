edc413cf118f289832661195af0f79f9496abafc
salah <salah@email.com>
2023-06-29 10:04:31 -0700

    done task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+**string_toupper - changes all lowercase letters of a string to uppercase
+*@s: string to be changed from lowcase to uppercase
+*Return: final result
+*/
+
+char *string_toupper(char *s)
+{
+int count = 0, i;
+
+while (s[count] != '\0')
+{
+count++;
+}
+i = count;
+while (i >= 0)
+{
+if (s[i] >= 97 && s[i] <= 122)
+{
+	s[i] = s[i] - 32;
+}
+else if (s[i] >= 65 && s[i] <= 90)
+{
+	s[i] = s[i] + 32;
+}
+i--;
+}
+return (s);
+
+}
