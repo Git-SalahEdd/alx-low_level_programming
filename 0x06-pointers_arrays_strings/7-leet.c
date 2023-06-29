549676e971443aa66959e78b94a8e3b1490bb797
salah <salah@email.com>
2023-06-29 12:25:11 -0700

    done task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,16 @@
 
 char *leet(char *s)
 {
-int y = 0, x = 0;
+int y = 0, x;
 char stg0[] = "aAeEoOtTlL";
 char stg00[] = "4433007711";
 
-for (; y < 10; y++)
+for (; s[y] != '\0'; y++)
 {
-	for(; x < 10; x++)
+	x = 0;
+	for (; x < 10; x++)
 	{
-		if(s[y] == stg0[x])
+		if (s[y] == stg0[x])
 		{
 			s[y] = stg00[x];
 		}
