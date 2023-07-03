a7f97fa514fe25b8fc6a805d1405598160bb0c77
salah <salah@email.com>
2023-07-03 06:04:08 -0700

    done task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+**_strchar -
+*@c:
+*@s:
+* Return:
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+		return (s + i);
+		}
+	}
+	return (NULL);
+}
+
