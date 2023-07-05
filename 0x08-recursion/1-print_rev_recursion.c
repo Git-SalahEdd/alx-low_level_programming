d1e1d7b82e413a26b84711b915367fcea85acd10
salah <salah@email.com>
2023-07-05 07:22:08 -0700

    done task

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+*_print_rev_recursion - prints a string in reverse
+*@s: string to be printed in reverse
+*Return:nothing
+*/
 
 void _print_rev_recursion(char *s)
 {
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,6 @@ int main(void);
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif
