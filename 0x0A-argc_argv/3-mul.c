bcf43dfb6aa8c2e5101b5c8c4e05ae5a3db5ab8d
salah <salah@email.com>
2023-07-07 09:50:19 -0700

    done task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2 || argc > 3)
+	int i;
+
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (argc == 3)
+	else if (argc == 2)
 	{
-		int i = atoi(argv[1]) * atoi(argv[2]);
-
+		i = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", i);
 	}
 	return (0);
