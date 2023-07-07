39ef2849fe007a5cf0b72f78441f8c78f6fc7610
salah <salah@email.com>
2023-07-07 09:08:22 -0700

    done task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*main - Entry point
+**@argc: argc int arg counter
+*@argv: argv char arg vector
+*Return: always 0 (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+	}
+	return (0);
+}
