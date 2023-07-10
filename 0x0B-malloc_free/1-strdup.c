d09f846424f9a70a98265839f726b816dae6e2dd
salah <salah@email.com>
2023-07-10 06:52:22 -0700

    done task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+**_strdup - copy of the string given as a parameter
+*@str: string to copy
+*Return: pointer
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	for ( ; str[sier] != '\0'; size++)
+		p = malloc(size * sizeof(*str) + 1);
+
+	if (p == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for ( ; i < size; i++)
+			p[i] = str[i];
+	}
+
+	return (p);
+}
