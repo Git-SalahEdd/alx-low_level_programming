d3a30ffd7b1c6edf7b9fd9ca6d2d9fb3a07f1f06
salah <salah@email.com>
2023-07-17 13:10:00 -0700

    done task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,8 +49,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int name_len;
 	int owner_len;
 
-	if (name != NULL && age > 0 && owner != NULL)
-	{
 	new_dog = (dog_t *) malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
@@ -75,10 +73,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(new_dog->name, name);
 	new_dog->age = age;
 	_strcpy(new_dog->owner, owner);
-	}
-	else
-	{
-		return (NULL);
-	}
 	return (new_dog);
 }
