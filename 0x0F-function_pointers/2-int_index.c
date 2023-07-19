2487029e391f7e5affb3b41ebf61459ba735773e
salah <salah@email.com>
2023-07-19 07:58:20 -0700

    done task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int x = 0;
 
-	if (array && size && cmp)
+	if (!(array == NULL || size <= 0 || cmp == NULL))
 		for (; x < size; x++)
 		{
 			if (cmp(array[x]))
