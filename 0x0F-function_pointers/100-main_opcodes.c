dfe374b9b5af3e389d3265674f9e7098f857d56b
salah <salah@email.com>
2023-07-20 01:39:41 -0700

    done task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main - main function
- * @argc: argc
- * @argv: argv
- * Return: 0
- */
-int main(int argc, char *argv[])
+*main - entry point print opcodes
+*@argc: arg count
+*@argv: arg array
+*Return: 0 always success
+*/
+
+int maint (int argc, char **argv)
 {
-	int i, n_bytes;
-	unsigned char *main_addr;
+	int b, i;
+	int (*add)(int, char **) = main;
+	unsigned char opcode;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	n_bytes = atoi(argv[1]);
 
-	if (n_bytes < 0)
+	b = atoi(argv[1]);
+
+	if (b < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	main_addr = (unsigned char *)main;
-	for (i = 0; i < n_bytes; i++)
+
+	for (i = 0; i < b; i++)
 	{
-		printf("0x%02x ", main_addr[i]);
+		opcode = *(unsigned char *)add;
+		printf("%.2", opcode);
+
+		if (i == b - 1)
+			continue;
+		printf(" ");
+
+		add++;
 	}
 	printf("\n");
 	return (0);
