c7a49467e969b9f2bfad9a2783a9ddc3074af6b7
salah <salah@email.com>
2023-07-27 10:39:06 -0700

    done task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
-void _constructor(void) __attribute__ ((constrator));
+void _constructor(void) __attribute__((constructor));
 
 /**
 *_constractor - exe before main() func
 */
 
-void _contractor(void)
+void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 		"I bore my house upon my back!\n");
