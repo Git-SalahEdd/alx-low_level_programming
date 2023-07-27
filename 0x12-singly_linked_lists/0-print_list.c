0d5beaff3eae08d526fa3318803c400ad108a52f
salah <salah@email.com>
2023-07-27 08:28:29 -0700

    done task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,15 +1,27 @@
 #include "lists.h"
+
+/**
+*_strlen - string len
+*@s: string
+*Return: len of str
+*/
+
 int _strlen(char *s)
 {
 	int i = 0;
 
-	if(!s)
+	if (!s)
 		return (0);
 	while (s[i] != '\0')
 		i++;
 	return (i);
 }
-/***/
+
+/**
+*print_list - print all element of a list
+*@list_t
+*Return: nbr of elements
+*/
 
 size_t print_list(const list_t *h)
 {
