42512accbe2eb3eda51bb04dfc3ddca66f8848b2
salah <salah@email.com>
2023-07-27 08:25:23 -0700

    done task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if(!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/***/
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
