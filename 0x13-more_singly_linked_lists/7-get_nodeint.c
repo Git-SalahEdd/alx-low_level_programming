36dd27cc4dd9ca70652fc84dab17a293a3752c6c
salah <salah@email.com>
2023-07-31 11:56:22 -0700

    done task

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,17 +12,17 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *node = head;
 
-	if (!node)
-		return (NULL);
-	while (node)
+	if (node)
 	{
-		i++;
-		if (i == index)
+		while (node)
 		{
-			return (node);
-			break;
+			if (i == index)
+			{
+				return (node);
+			}
+			node = node->next;
+			i++;
 		}
-		node = node->next;
 	}
 	return (node);
 }
