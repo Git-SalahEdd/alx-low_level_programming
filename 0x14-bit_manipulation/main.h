933aef736996dafac5e204dbb8ffe03ecb4b6a70
salah <salah@email.com>
2023-08-03 11:31:44 -0700

    done task

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ unsigned int binary_to_uint(const char *b);
 int _power(int x, int y);
 int _atoi(const char *s);
 void print_binary(unsigned long int n);
-
+int get_bit(unsigned long int n, unsigned int index);
+int get_bit_rsv(unsigned long int n, unsigned int index);
 #endif
