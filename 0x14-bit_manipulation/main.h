3b289bc477d7ebccf4dce914133ae86ff36c48da
salah <salah@email.com>
2023-08-03 10:39:49 -0700

    done task

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+int _power(int x, int y);
+int _atoi(const char *s);
+#endif
