e195674c8222a84375bf8b420aea1cd12107f754
salah <salah@email.com>
2023-08-07 11:40:24 -0700

    done task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,11 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t len = 0, bytes = 0;
 
-	while (*t_c++)
+	if (t_c)
+	{
+	while (t_c[len])
 		len++;
-
+	}
 	if (!filename)
 		return (-1);
 
@@ -23,10 +25,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (len)
-		bytes = write(fd, text_content, len);
+		bytes = write(fd, t_c, len);
 	if (bytes == -1)
 	{
-		close(fd);
 		return (-1);
 	}
 	close(fd);
