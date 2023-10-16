147d403a68150742014edc3867707877799ac5dd
Salah Eddine <shen.real@live.fr>
2023-10-16 03:39:53 -1200

    done task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-LD_PRELOAD=/$PWD/libinjc.so
+wget -P /tmp https://github.com/Git-SalahEdd/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libinjc.so
+export LD_PRELOAD=/tmp/libinjc.so
