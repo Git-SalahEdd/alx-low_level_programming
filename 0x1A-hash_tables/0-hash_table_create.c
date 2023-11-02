bd07d962e080975af83ce592d10aabc0f2851857
Salah Eddine <shen.real@live.fr>
2023-11-02 01:45:27 -1200

    done task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
@@ -18,12 +17,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!new_table->array)
-	{
-		free(new_table);
 		return (NULL);
-	}
-
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
 	return (new_table);
 }
